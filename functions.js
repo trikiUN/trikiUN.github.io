1fbb6bfe50c00be55e67a6e5d5e6dc2ce13f00d5
Unknown <dicrojasch@unal.edu.co>
2017-09-30 17:28:52 -0500

    Add first move, same probability to start the player or the agent

--- functions.js
+++ functions.js
@@ -1,11 +1,17 @@
 var matrix = [ null, null, null, null, null, null, null, null, null ]
 
 var turnCounter = 0;
+var startPlayer = 0;
 
 var start = function(){
+  startPlayer = parseInt(Math.random()*10)%2; // choose player : 0=player , 1=agent
   document.getElementById("start").style.display = "none";
   document.getElementById("game").style.display = "initial";
   document.getElementById("score").style.display = "block";
+
+  if( startPlayer == 1 ){
+     moveAgent((parseInt(Math.random()*10)%9+1)+"2");
+  }
 
 }
 var clickedButton = function ( id ){
@@ -14,8 +20,8 @@ var clickedButton = function ( id ){
 }
 
 var moveAgent = function ( id ){
-  document.getElementById(id+1).style.display = "initial";
-  document.getElementById(id-1).style.display = "none";
+  document.getElementById(id).style.display = "initial";
+  document.getElementById(id-2).style.display = "none";
 }
 
 
