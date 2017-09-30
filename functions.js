44ab8d6979bc96b044c55d803e4e07576be143b0
Unknown <dicrojasch@unal.edu.co>
2017-09-30 15:00:30 -0500

    Fix reponsive on mobile phone

--- functions.js
+++ functions.js
@@ -4,7 +4,7 @@ var turnCounter = 0;
 
 var clickedButton = function ( id ){
   document.getElementById(id).style.display = "initial";
-  document.getElementById(id-1).style.display = "none";
+  document.getElementById(id-1).style.display = "none";    
 }
 
 var playMachine = function(  ) {}
