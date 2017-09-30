ba7debfa367e2162cadd1fe38394555ee6497df1
Unknown <dicrojasch@unal.edu.co>
2017-09-30 15:42:23 -0500

    Add start button

--- functions.js
+++ functions.js
@@ -2,9 +2,22 @@ var matrix = [ null, null, null, null, null, null, null, null, null ]
 
 var turnCounter = 0;
 
+var start = function(){
+  document.getElementById("start").style.display = "none";
+  document.getElementById("game").style.display = "initial";
+  document.getElementById("score").style.display = "block";
+
+}
 var clickedButton = function ( id ){
   document.getElementById(id).style.display = "initial";
-  document.getElementById(id-1).style.display = "none";    
+  document.getElementById(id-1).style.display = "none";
 }
+
+var moveAgent = function ( id ){
+  document.getElementById(id+1).style.display = "initial";
+  document.getElementById(id-1).style.display = "none";
+}
+
+
 
 var playMachine = function(  ) {}
