0471997d17103f483bf6905d882cc74a77f576b9
Unknown <dicrojasch@unal.edu.co>
2017-09-30 18:32:16 -0500

    Add check who wins in each move

--- functions.js
+++ functions.js
@@ -11,11 +11,16 @@ var start = function(){
   if( startPlayer == 1 )
      moveAgent((parseInt(Math.random()*10)%9+1)+"2");  // Math.random()*10)%9 = number 0-8  -   +1 = because index in the game are 1-9
 }
+
+
 var clickedButton = function ( id ){
   if( matrix[parseInt(id/10)] == null ){
     document.getElementById(id).style.display = "initial";
     document.getElementById(id-1).style.display = "none";
-    matrix[parseInt(id/10)] = true;
+    matrix[parseInt(id/10)] = 0;
+    if( checkWinner() == true ){
+      alert("You win")
+    }
   }
 
 }
@@ -24,10 +29,49 @@ var moveAgent = function ( id ){
   if( matrix[parseInt(id/10)] == null ){
     document.getElementById(id).style.display = "initial";
     document.getElementById(id-2).style.display = "none";
-    matrix[parseInt(id/10)] = true;    
+    matrix[parseInt(id/10)] = 1;
+    if( checkWinner() == true ){
+      alert("You lost")
+    }
   }
 
 }
+
+var checkWinner = function (){
+  var temp = true;
+  for(var i=1; i<4; i++){ // check vertical
+    temp = true
+    temp = temp && (matrix[i] != null) && (matrix[i+3] != null) && (matrix[i+6] != null);
+    temp = temp && (matrix[i] == matrix[i+3]) && (matrix[i+3] == matrix[i+6]);
+
+    if( temp == true )
+      return true;
+  }
+
+  for(var i=1; i<8; i=i+3){ // check horizontal
+    temp = true
+    temp = temp && (matrix[i] != null) && (matrix[i+1] != null) && (matrix[i+2] != null);
+    temp = temp && (matrix[i] == matrix[i+1]) && (matrix[i+1] == matrix[i+2]);
+    if( temp == true )
+      return true;
+  }
+  // check lateral
+  var i = 1;
+  temp = true
+  temp = temp && (matrix[i] != null) && (matrix[i+4] != null) && (matrix[i+8] != null);
+  temp = temp && (matrix[i] == matrix[i+4]) && (matrix[i+4] == matrix[i+8]);
+  if( temp == true )
+    return true;
+  // check lateral
+  var i = 3;
+  temp = true
+  temp = temp && (matrix[i] != null) && (matrix[i+2] != null) && (matrix[i+4] != null);
+  temp = temp && (matrix[i] == matrix[i+2]) && (matrix[i+2] == matrix[i+4]);
+  if( temp == true )
+    return true;
+  return false;
+}
+
 
 
 
