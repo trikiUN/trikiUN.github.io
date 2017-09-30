412e8cbe289a1351996020b321dbf8ba399c3d68
Unknown <dicrojasch@unal.edu.co>
2017-09-30 12:42:57 -0500

    Initial game interface

---
+++ functions.js
@@ -0,0 +1,10 @@
+var matrix = [ null, null, null, null, null, null, null, null, null ]
+
+var turnCounter = 0;
+
+var clickedButton = function ( id ){
+  document.getElementById(id).style.display = "initial";
+  document.getElementById(id-1).style.display = "none";
+}
+
+var playMachine = function(  ) {}
